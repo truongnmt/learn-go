5937dbe6fd9be512d6e89f9067d6337570f3c19e
TruongNM <nguyen.manh.truong@mf-financial.jp>
2018-11-21 13:38:43 +0900

    mock

--- di.go
+++ di.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer,"Hello, %s", name)
 }
 
-func main() {
-	Greet(os.Stdout, "Elodie")
-}
+//func main() {
+//	Greet(os.Stdout, "Elodie")
+//}
